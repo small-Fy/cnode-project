7ce5dd8028d64c4714464c73670649aecbd4e1c7
AaShun <1341393127@qq.com>
2019-10-21 11:10:55 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store/index";
+import axios from "axios";
+import http from "./http";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.prototype.$axios = http;
+Vue.config.productionTip = false;
+
+// 响应拦截
+axios.defaults.timeout = 10000; // 设置超时时间
+// 过滤请求结果 只返回结果的data
+axios.interceptors.response.use(
+  response => {
+    return response.data;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
